1f2c5cbc9804de7cdf1716c809c21e6f153bd31b
nagalil-hanoj-hteal3 <96222174+nagalil-hanoj-hteal3@users.noreply.github.com>
2024-07-19 23:58:18 +0000

    new changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,8 @@
 
 echo "==================="
 
-# keep track of who is running the action (logging)
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
-
-# to run git operations
 git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
